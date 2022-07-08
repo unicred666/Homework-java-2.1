aff40b2e05c777641bb9dab8333a05b9b0ffe157
IliyaMats <varchyn207@gmail.com>
2022-07-08 20:00:45 +0300

    add project miles

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+      int ticketPrice = 15_000;
+        int milePrice = 20;
+        int milesEarned = ticketPrice / milePrice;
+        System.out.println(milesEarned);
+    }
+}
